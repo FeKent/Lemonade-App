2e6d5b463e2d3ea13655b7751c005f9fc8cac37a
SnippyRex <kentf022@gmail.com>
2023-05-18 01:12:58 +0100

    Add a Scaffold top bar

--- app/src/main/java/com/example/lemonade_app/MainActivity.kt
+++ app/src/main/java/com/example/lemonade_app/MainActivity.kt
@@ -16,8 +16,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade_app.ui.theme.LemonadeAppTheme
 import java.util.IdentityHashMap
 
@@ -42,12 +44,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LemonadeApp() {
 
-    Scaffold(topBar = { TopAppBar(title = {"Lemonade"}, backgroundColor = Color.Yellow)
-    }, content = LemonScreen(
+    Scaffold(topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.top_bar), fontSize = 30.sp, color = Color.Black, textAlign = TextAlign.Center
+    )}, backgroundColor = Color.Yellow)
+    }, content = { Text(text = "") }
+    )
+    LemonScreen(
         textLabelResourceId = R.string.lemon_select,
         drawableResourceId = R.drawable.lemon_tree ,
         contentDescriptionId = R.string.tree_content_description,
-    )
     )
 }
 
@@ -58,10 +62,10 @@ fun LemonScreen(
     drawableResourceId: Int,
     contentDescriptionId: Int,
     modifier: Modifier = Modifier
-): @Composable (PaddingValues) -> Unit {
+) {
     Box(
         modifier = modifier
-    ){
+    ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
@@ -71,13 +75,13 @@ fun LemonScreen(
 //                onClick = onImageClick,
 //                shape = RoundedCornerShape( 40.dp)
 //            ){
-                Image(
-                    painter = painterResource(drawableResourceId),
-                    contentDescription = stringResource(contentDescriptionId),
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .padding(24.dp)
-                )
+            Image(
+                painter = painterResource(drawableResourceId),
+                contentDescription = stringResource(contentDescriptionId),
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(24.dp)
+            )
 //            }
             Spacer(modifier = Modifier.height(32.dp))
             Text(text = stringResource(textLabelResourceId))
