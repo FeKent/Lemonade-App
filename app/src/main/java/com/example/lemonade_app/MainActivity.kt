5c072f5042c47079d856a81f5a647415e75c7403
SnippyRex <kentf022@gmail.com>
2023-05-19 16:58:52 +0100

    Add Material3 dependencies to align TopBar & move Image into Button

--- app/src/main/java/com/example/lemonade_app/MainActivity.kt
+++ app/src/main/java/com/example/lemonade_app/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.example.lemonade_app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lemonade_app.ui.theme.LemonadeAppTheme
-import java.util.IdentityHashMap
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,18 +39,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview (showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Preview(showBackground = true)
 @Composable
 fun LemonadeApp() {
 
-    Scaffold(topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.top_bar), fontSize = 30.sp, color = Color.Black, textAlign = TextAlign.Center
-    )}, backgroundColor = Color.Yellow)
-    }, content = { Text(text = "") }
-    )
-    LemonScreen(
-        textLabelResourceId = R.string.lemon_select,
-        drawableResourceId = R.drawable.lemon_tree ,
-        contentDescriptionId = R.string.tree_content_description,
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(id = R.string.top_bar),
+                    fontSize = 30.sp,
+                    color = Color.Black
+                )
+            },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = Color(249,228,76,255)),
+        )
+    }, content = {
+        LemonScreen(
+            textLabelResourceId = R.string.lemon_select,
+            drawableResourceId = R.drawable.lemon_tree,
+            contentDescriptionId = R.string.tree_content_description,
+        )
+    }
     )
 }
 
@@ -71,18 +82,19 @@ fun LemonScreen(
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-//            Button(
-//                onClick = onImageClick,
-//                shape = RoundedCornerShape( 40.dp)
-//            ){
-            Image(
-                painter = painterResource(drawableResourceId),
-                contentDescription = stringResource(contentDescriptionId),
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(24.dp)
-            )
-//            }
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(195, 236, 219, 255)),
+                shape = RoundedCornerShape(48.dp)
+            ) {
+                Image(
+                    painter = painterResource(drawableResourceId),
+                    contentDescription = stringResource(contentDescriptionId),
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(24.dp)
+                )
+            }
             Spacer(modifier = Modifier.height(32.dp))
             Text(text = stringResource(textLabelResourceId))
         }
